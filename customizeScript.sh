034cd172ac90dfbc968daecf4bb141de9ff6e136
Mat Morgan <mamorga@microsoft.com>
2020-09-02 10:46:11 -0700

    updates to script

--- customizeScript.sh
+++ customizeScript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
-
+# add epel repo & update
+sudo yum install -y epel-release && sudo yum update -y
 # Add repo key for az cli
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
@@ -12,7 +13,39 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
 # install az cli
-sudo yum install azure-cli --assumeyes
+sudo yum install -y azure-cli
+
+#install singularity
+sudo yum install -y singularity
+
+#install python3
+sudo yum install python3
+
+#install R
+sudo yum install -y R
+
+#install SAMTools 
+sudo yum install -y samtools
+
+#download azcopy v7 & v10, use v7 installer script to install v10 & add to path
+wget -O azcopy_v7.tar.gz https://aka.ms/downloadazcopylinux64
+mkdir azcopy_v7
+tar -xf azcopy_v7.tar.gz -C azcopy_v7
+wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux
+mkdir azcopy_v10
+tar -xf azcopy_v10.tar.gz -C azcopy_v10
+# this step may fail with updates, the _amd64_10.x.x version path will change
+mv azcopy_v10/azcopy_linux_amd64_10.6.0 azcopy_v10/azcopy
+cp azcopy_v7/install.sh azcopy_v10/install.sh
+cp azcopy_v7/azcopy/azcopy_autocomplete azcopy_v10/azcopy/
+cp azcopy_v7/azcopy/startup azcopy_v10/azcopy/
+sudo ./azcopy_v10/install.sh
+#tmux
+sudo yum install -y tmux
+#java 11
+sudo yum install -y java-11-openjdk
+
+
 
 # Add preview banner to MOTD
 cat >> /etc/motd << EOF
